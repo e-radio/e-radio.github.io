8dd0253b0c57994f0b15fd7191dc41f9c6a8f137
30690 <user.email=187264029+30690@users.noreply.github.com>
2026-02-03 14:03:40 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-      output: "static",
+  output: "static",
+  site: "https://e-radio.github.io",
+  integrations: [sitemap()],
 });
